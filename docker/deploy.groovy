817dba8709f9177d2aae253ac934581497227d4c
coryk2049 <cory.kin@gmail.com>
2019-03-05 20:09:43 -0500

    Revision

--- docker/deploy.groovy
+++ docker/deploy.groovy
@@ -4,14 +4,17 @@
 */
 
 def call() {
-  stage "Deploying Docker Image", {
+  stage "Deploying Docker Image Locally", {
     node{
       unstash "workspace"
       login_to_registry()
       def images = get_images_to_build()
       println("${images}")
-      def img = images.find { it == 'repo:spring-boot-api' }
-      println("${img}")
+      def img = images.find { it.repo == 'spring-boot-api' }
+      println("${img.repo}")
+      if ("${img.repo}" != null) {
+        sh "docker run --rm -it -p 8088:8080 --network=try-it-out_sdp --name spring-boot-api ${img.registry}/${img.repo}:${img.tag}"
+      }
     }
   }
 }
